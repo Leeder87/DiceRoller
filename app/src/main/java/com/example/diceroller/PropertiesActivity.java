ea9d7f881e6efc48d4ef5d58f2dce12a757fa583
Leeder87 <leeder@list.ru>
2020-07-20 16:43:06 +0300

    Отключение звука по настройке. Отладил механизм работы с настройками

--- app/src/main/java/com/example/diceroller/PropertiesActivity.java
+++ app/src/main/java/com/example/diceroller/PropertiesActivity.java
@@ -3,23 +3,35 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class PropertiesActivity extends AppCompatActivity {
     private Button btnBack, btnInfo;
+    private SeekBar seekBarSpeed, seekBarNoR;
+    private Switch switchSound;
+
+    private static final String PREFS_FILE = "DicerollerPrefs";
+    private static final String PREF_SOUND = "sound";
+    private static final String PREF_NOR = "nor";
+    private static final String PREF_SPEED = "speed";
+    SharedPreferences settings;
+    SharedPreferences.Editor prefEditor;
 
     @Override
-        //назовем это телом
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_properties);
+        settings = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
 
         setButtons();
     }
-        //Кнопка назад
+
+    //Кнопка назад
     private void setButtons() {
         btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(
@@ -30,7 +42,8 @@ public class PropertiesActivity extends AppCompatActivity {
                     }
                 }
         );
-    //Кнопка Info
+
+        //Кнопка Info
         btnInfo = findViewById(R.id.btnInfo);
         btnInfo.setOnClickListener(
                 new View.OnClickListener() {
@@ -41,6 +54,27 @@ public class PropertiesActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        seekBarSpeed = findViewById(R.id.seekBarSpeed);
+        seekBarNoR = findViewById(R.id.seekBarNoR);
+        switchSound = findViewById(R.id.switchSound);
+        // получаем настройки
+        boolean soundOn = settings.getBoolean(PREF_SOUND, true);
+        switchSound.setChecked(soundOn);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        //EditText nameBox = (EditText) findViewById(R.id.nameBox);
+        //String name = nameBox.getText().toString();
+
+        boolean soundOn = switchSound.isChecked();
+        // сохраняем в настройках
+        prefEditor = settings.edit();
+        prefEditor.putBoolean(PREF_SOUND, soundOn);
+        prefEditor.apply();
     }
 
 }
