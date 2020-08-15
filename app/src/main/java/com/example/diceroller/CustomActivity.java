4470f5c0ce3ad5bdb6f869adb31d9529a4f8bf07
Leeder87 <leeder@list.ru>
2020-08-16 02:15:16 +0300

    В CustomActivity добавлены три кнопки Memory и обработчики этих кнопок

--- app/src/main/java/com/example/diceroller/CustomActivity.java
+++ app/src/main/java/com/example/diceroller/CustomActivity.java
@@ -22,24 +22,28 @@ import android.view.animation.AnimationUtils;
 import android.media.MediaPlayer;
 
 import java.util.Date;
+import java.util.prefs.Preferences;
 
 public class CustomActivity extends AppCompatActivity {
-    private Button btnBack, btnRoll, btnInfo, btnMiniHistory;
+    private Button btnBack, btnRoll, btnInfo, btnMiniHistory, btnMem1, btnMem2, btnMem3;
+    private EditText editFormula;
     private MediaPlayer SelectSound;
     private static final String PREFS_FILE = "DicerollerPrefs";
     private static final String PREF_SOUND = "sound";
     boolean soundOn;
     SharedPreferences settings;
+    SharedPreferences thisSettings;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
         setButtonRoll();
-        setButtons(); // Вызов метода, привязывающего к кнопкам обработчики
         SelectSound = MediaPlayer.create(this, R.raw.select_menu);
         // получаем первоначальные настройки
         settings = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
+        thisSettings = getPreferences(MODE_PRIVATE);
         soundOn = settings.getBoolean(PREF_SOUND, true);
         setButtons(); // Вызов метода, привязывающего к кнопкам обработчики
     }
@@ -47,6 +51,10 @@ public class CustomActivity extends AppCompatActivity {
         super.onResume();
         // переполучаем настройки
         soundOn = settings.getBoolean(PREF_SOUND, true);
+        // при перезагрузке активити надо обновить надписи на кнопках
+        setMemoryButtons(btnMem1);
+        setMemoryButtons(btnMem2);
+        setMemoryButtons(btnMem3);
         /*Toast.makeText(
                 MainActivity.this,
                 "soundOn: " + String.valueOf(soundOn),
@@ -62,9 +70,76 @@ public class CustomActivity extends AppCompatActivity {
     // Метод для настройки обработчиков кнопок
     private void setButtons() {
         final Animation btnScale = AnimationUtils.loadAnimation(this, R.anim.scale);
+        editFormula = findViewById(R.id.editFormula);
         btnBack = findViewById(R.id.btnBack);
         btnInfo = findViewById(R.id.btnInfo);
         btnMiniHistory = findViewById(R.id.btnMiniHistory);
+        btnMem1 = findViewById(R.id.btnMem1);
+        btnMem2 = findViewById(R.id.btnMem2);
+        btnMem3 = findViewById(R.id.btnMem3);
+
+        btnMem1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setMemoryButtons(btnMem1);
+                        String formula = btnMem1.getText().toString();
+                        if (!formula.equals("Memory"))
+                            StartParse(view, formula);
+                    }
+                }
+        );
+        btnMem1.setOnLongClickListener(
+                new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        flushMemoryButton(btnMem1);
+                        return true;
+                    }
+                }
+        );
+
+        btnMem2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setMemoryButtons(btnMem2);
+                        String formula = btnMem2.getText().toString();
+                        if (!formula.equals("Memory"))
+                            StartParse(view, formula);
+                    }
+                }
+        );
+        btnMem2.setOnLongClickListener(
+                new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        flushMemoryButton(btnMem2);
+                        return true;
+                    }
+                }
+        );
+
+        btnMem3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setMemoryButtons(btnMem3);
+                        String formula = btnMem3.getText().toString();
+                        if (!formula.equals("Memory"))
+                            StartParse(view, formula);
+                    }
+                }
+        );
+        btnMem3.setOnLongClickListener(
+                new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        flushMemoryButton(btnMem3);
+                        return true;
+                    }
+                }
+        );
 
         btnBack.setOnClickListener(
                 new View.OnClickListener() {
@@ -121,40 +196,98 @@ public class CustomActivity extends AppCompatActivity {
 
     // Настраиваем обработчик кнопки Roll
     private void setButtonRoll() {
-        final EditText editFormula = findViewById(R.id.editFormula);
-        final TextView txtResult = findViewById(R.id.txtResultRoll);
-        final Animation btnScale = AnimationUtils.loadAnimation(this, R.anim.scale);
         btnRoll = findViewById(R.id.btnRoll);
 
         btnRoll.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        soundPlay(SelectSound);
-                        view.startAnimation(btnScale);
                         String formula = editFormula.getText().toString();
-                        // Создаём парсер
-                        FormulaParser parser = new FormulaParser(formula);
-                        if (!formula.equals("")) {
-                            try {
-                                // Парсим, в случае исключения перехватываем его и показываем ошибку
-                                parser.parse();
-                            } catch (Exception ex) {
-                                // Тут надо будет залогировать исключение
-                                parser.result = "Error";
-                            }
-                        }
-                        else
-                            parser.result = "Enter a throw formula:";
-
-                        txtResult.setText(parser.result);
-
-                        // Подготавливаем запись для истории бросков
-                        HistoryRecord historyRecord = new HistoryRecord(new Date(), parser.formula,
-                                parser.prettyFormula, parser.result, "Custom");
-                        DatabaseHelper.insertRecord(getBaseContext(), historyRecord);
+                        StartParse(view, formula);
                     }
                 }
         );
+    }
+
+    private void StartParse(View view, String formula) {
+        final TextView txtResult = findViewById(R.id.txtResultRoll);
+        final Animation btnScale = AnimationUtils.loadAnimation(this, R.anim.scale);
+        soundPlay(SelectSound);
+        view.startAnimation(btnScale);
+        // Создаём парсер
+        FormulaParser parser = new FormulaParser(formula);
+        if (!formula.equals("")) {
+            try {
+                // Парсим, в случае исключения перехватываем его и показываем ошибку
+                parser.parse();
+            } catch (Exception ex) {
+                // Тут надо будет залогировать исключение
+                parser.result = "Error";
+            }
+        }
+        else
+            parser.result = "Enter a throw formula:";
+
+        txtResult.setText(parser.result);
+
+        // Подготавливаем запись для истории бросков
+        HistoryRecord historyRecord = new HistoryRecord(new Date(), parser.formula,
+                parser.prettyFormula, parser.result, "Custom");
+        DatabaseHelper.insertRecord(getBaseContext(), historyRecord);
+    }
+
+    // Настраиваем обработчики кнопок Memory
+    private void setMemoryButtons(Button btn) {
+        // Получаем настройку из настроек
+        String btnMemText = thisSettings.getString(btn.getResources().getResourceEntryName(btn.getId()), "");
+        assert btnMemText != null;
+        if (btnMemText.isEmpty()) {
+            // Если настройка пустая, то получаем текст из строки формулы
+            String formula = editFormula.getText().toString();
+            if (!formula.isEmpty()) {
+                // если строка формулы не пустая, то
+                // меняем текст на кнопке и сохраняем в настройках
+                btn.setText(formula);
+                prefEditor = thisSettings.edit();
+                prefEditor.putString(btn.getResources().getResourceEntryName(btn.getId()), formula);
+                prefEditor.apply();
+            }
+        }
+        else {
+            btn.setText(btnMemText);
+            // Тут нельзя сразу парсить, потому что при перезагрузке страницы
+            // сразу будет новый результат. Поэтому этот вызов пришлось перенести
+            // в обработчик нажатия на каждую из кнопок.
+            //StartParse(btn, btnMemText);
+        }
+    }
+
+    // Сбрасываем обработчики и текст кнопок Memory
+    private void flushMemoryButton(Button btn) {
+        final Button btn1 = btn;
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(CustomActivity.this, R.style.AlertDialogStyle);
+        a_builder.setMessage("Are you sure you want to clean this memory button?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        soundPlay(SelectSound);
+                        btn1.setText(R.string.memorize);
+                        prefEditor = thisSettings.edit();
+                        prefEditor.remove(btn1.getResources().getResourceEntryName(btn1.getId()));
+                        prefEditor.apply();
+                        dialogInterface.cancel();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        soundPlay(SelectSound);
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Erase memory button");
+        alert.show();
     }
 }
